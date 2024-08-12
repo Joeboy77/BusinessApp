e060e4a39f6a4c4a1cf452ae0b9c62fc28f8114f
Joseph Acheampong <acheampongjoseph470@gmail.com>
2024-08-12 23:16:58 +0000

    authentication added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,13 +6,29 @@ import * as SplashScreen from 'expo-splash-screen';
 import React, { useEffect } from 'react';
 import 'react-native-reanimated';
 import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo'
-
-
 import { useColorScheme } from '@/hooks/useColorScheme';
 import LoginScreen from '@/components/LoginScreen';
+import * as SecureStore from 'expo-secure-store'
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
+
 SplashScreen.preventAutoHideAsync();
+
+const tokenCache = {
+  async getToken(key: string){
+    try{
+      return SecureStore.getItemAsync(key)
+    } catch(err){
+      return null
+    }
+  },
+  async saveToken(key: string, value: string){
+    try{
+      return SecureStore.setItemAsync(key, value)
+    } catch(err) {
+      return
+    }
+  }
+}
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -35,7 +51,7 @@ export default function RootLayout() {
   })
 
   return (
-    <ClerkProvider publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <SignedIn>
           <Stack>
