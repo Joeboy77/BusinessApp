c637f8cd7ffdb77bf5e206a5b01a19b31a69ba00
Joseph Acheampong <acheampongjoseph470@gmail.com>
2024-08-12 22:41:59 +0000

    update

--- components/LoginScreen.tsx
+++ components/LoginScreen.tsx
@@ -1,10 +1,48 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { Colors } from '@/constants/Colors'
 
 export default function LoginScreen() {
   return (
     <View>
-      
+        <View style={styles.viewOne}>
+            <View style={styles.contSub}></View>
+        </View>
+        <View style={styles.viewTwo}>
+            <Text style={{fontSize: 25, fontFamily: 'outfit-bold', textAlign: 'center', fontWeight: '700'}}>Your Ultimate 
+                <Text style={{color: Colors.PRIMARY, }}> Comminity Business Directory</Text> App
+            </Text>
+            <Text style={{fontSize: 15, fontFamily: 'outfit', textAlign: 'center', marginVertical: 15, color: Colors.GRAY}}>Find your favorite business near and post your own business</Text>
+            <TouchableOpacity style={styles.btn}>
+                <Text style={{textAlign: 'center',
+                    color: '#fff', fontFamily: 'outfit-bold', fontWeight: '700', fontSize: 16
+                }}>Let's Get Started</Text>
+            </TouchableOpacity>
+        </View>
     </View>
   )
 }
+const styles = StyleSheet.create({
+    contSub: {
+        width: 250,
+        height: 450,
+        backgroundColor: 'plum',
+        borderRadius: 20,
+        borderWidth: 6,
+    },
+    viewOne: {
+        display: 'flex',
+        alignItems: 'center',
+        marginTop: 100
+    },
+    viewTwo: {
+        backgroundColor: '#fff',
+        padding: 20
+    },
+    btn: {
+        backgroundColor: Colors.PRIMARY,
+        padding: 16,
+        borderRadius: 99,
+        marginTop: 20
+    }
+})
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -23,4 +23,6 @@ export const Colors = {
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
   },
+ PRIMARY: '#7F57F1',
+ GRAY: '#8F8F8F'
 };
