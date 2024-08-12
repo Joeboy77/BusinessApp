2ca990769e89436a3f72937e965fe415b8ca0c3c
Joseph Acheampong <acheampongjoseph470@gmail.com>
2024-08-12 22:00:29 +0000

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,15 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Text } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import React, { useEffect } from 'react';
 import 'react-native-reanimated';
-import { ClerkProvider } from '@clerk/clerk-expo'
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo'
 
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import LoginScreen from '@/components/LoginScreen';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -33,11 +35,16 @@ export default function RootLayout() {
   })
 
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
+        <SignedIn>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          </Stack>
+        </SignedIn>
+        <SignedOut>
+          <LoginScreen />
+        </SignedOut>
       </ThemeProvider>
     </ClerkProvider>
   );
