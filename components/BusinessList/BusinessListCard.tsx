82244606112a060386bf4ac3905e85c24f558b44
Joseph Acheampong <acheampongjoseph470@gmail.com>
2024-08-13 12:56:56 +0000

    update

--- components/BusinessList/BusinessListCard.tsx
+++ components/BusinessList/BusinessListCard.tsx
@@ -1,11 +1,23 @@
 import { View, Text, Image, StyleSheet} from 'react-native'
 import React from 'react'
+import star from '../../assets/images/star.png'
+import { ThemedText } from '../ThemedText'
+import { ThemedView } from '../ThemedView'
+import { Colors } from '@/constants/Colors'
 
 export default function BusinessListCard({business}) {
   return (
-    <View style={styles.container}>
+    <ThemedView style={styles.container}>
       <Image  source={{uri:business.imageUrl}} style={styles.image}/>
-    </View>
+      <ThemedView style={{flex: 1, gap: 5}}>
+        <ThemedText style={{fontFamily: 'outfit-bold', fontSize: 19}}>{business.name}</ThemedText>
+        <ThemedText style={{fontFamily: 'outfit', color: Colors.GRAY}}>{business.address}</ThemedText>
+        <ThemedView style={{display: 'flex', flexDirection: 'row', gap: 5, alignItems: 'center'}}>
+                <Image source={star} style={{width: 15, height: 15}}/>
+                <ThemedText style={{fontFamily: 'outfit', color: '#fff', fontSize: 15}}>4.5</ThemedText>
+            </ThemedView>
+      </ThemedView>
+    </ThemedView>
   )
 }
 const styles = StyleSheet.create({
@@ -18,6 +30,8 @@ const styles = StyleSheet.create({
         padding: 10,
         margin: 10,
         borderRadius: 15,
-        backgroundColor: '#fff'
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 10
     }
 })
