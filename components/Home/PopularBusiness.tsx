bba297f3095573e21796011c8ca61125a74997b5
Joseph Acheampong <acheampongjoseph470@gmail.com>
2024-08-13 02:42:32 +0000

    update

--- components/Home/PopularBusiness.tsx
+++ components/Home/PopularBusiness.tsx
@@ -1,10 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Colors } from '@/constants/Colors'
+import { collection, getDocs, limit, query } from 'firebase/firestore'
+import { db } from '@/configs/FirebaseConfig'
+
 
 export default function PopularBusiness() {
+
+    const [businessList, setBusinessList] = useState([])
+
+    useEffect(() => {
+        GetBusinessList()
+    }, [])
+    const GetBusinessList = async() => {
+        setBusinessList([])
+        const q = query(collection(db, 'BusinessList'), limit(10))
+        const querySnapshot = await getDocs(q)
+
+        querySnapshot.forEach((doc) => {
+            console.log(doc.data());
+            setBusinessList(prev => [...prev,doc.data()])
+        })
+    }
+
   return (
     <View>
-      <Text>PopularBusiness</Text>
+      <View style={styles.sub}>
+        <Text style={{color: '#fff',  fontSize: 20, fontFamily: 'outfit-bold'}}>Popular Business
+        </Text>
+        <Text style={{color: Colors.PRIMARY, fontFamily: 'outfit-meduim'}}>View all</Text>
+      </View>
     </View>
   )
 }
+const styles = StyleSheet.create({
+    sub: {
+        padding: 20,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 10,
+    }
+})
