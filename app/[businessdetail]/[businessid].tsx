5ac3c8d541d1bdb9e049f92b96d6c5cad44dca82
Joseph Acheampong <acheampongjoseph470@gmail.com>
2024-08-13 15:14:04 +0000

    update

--- app/[businessdetail]/[businessid].tsx
+++ app/[businessdetail]/[businessid].tsx
@@ -1,17 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { useLocalSearchParams } from 'expo-router'
 import { ThemedText } from '@/components/ThemedText'
+import { collection, doc, getDoc } from '@firebase/firestore'
+import {db} from '../../configs/FirebaseConfig'
+import { Colors } from '@/constants/Colors'
 
 export default function BusinessDetail() {
-
+    const [business, setBusiness] = useState()
+    const [loading, setLoading] = useState(false)
     const {businessid} = useLocalSearchParams()
-    const GetBusinessDetailById = () => {
-        
+
+    useEffect(() => {
+        GetBusinessDetailById()
+    }, [])
+
+    const GetBusinessDetailById = async() => {
+        setLoading(true)
+        const docRef:any = doc(db, 'BusinessList', businessid)
+        const docSnap= await getDoc(docRef)
+        if(docSnap.exists()) {
+            setBusiness(docSnap.data())
+            setLoading(false)
+        } else {
+            console.log("No such document");
+            
+        }
     }
   return (
     <View>
-      <ThemedText>{businessid}</ThemedText>
+        {loading?
+        <ActivityIndicator size={'large'} color={Colors.PRIMARY} style={{marginTop: '70%'}}/>:
+        <View>
+            <ThemedText>{businessid}</ThemedText>
+        </View>
+        }
+      
     </View>
   )
 }
