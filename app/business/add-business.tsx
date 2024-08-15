c8e90763cec599555ad557aa9bba959e1e889c6a
Joseph Acheampong <acheampongjoseph470@gmail.com>
2024-08-15 01:51:56 +0000

    update

--- app/business/add-business.tsx
+++ app/business/add-business.tsx
@@ -1,21 +1,34 @@
-import { View, StyleSheet, Image, TouchableOpacity, TextInput} from 'react-native'
+import { View, StyleSheet, Image, TouchableOpacity, TextInput, ToastAndroid} from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useNavigation } from 'expo-router'
 import { ThemedText } from '@/components/ThemedText'
 import { Colors } from '@/constants/Colors'
 import * as ImagePicker from 'expo-image-picker';
 import RNPickerSelect from 'react-native-picker-select'
+import { collection, doc, getDocs, query, setDoc, snapshotEqual } from 'firebase/firestore'
+import { db, storage } from '@/configs/FirebaseConfig'
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
+import { useUser } from '@clerk/clerk-expo'
 
 
 export default function AddBusiness() {
   const navigation = useNavigation()
   const [image, setImage] = useState(null)
-  const [selectedLanguage, setSelectedLanguage] = useState();
+  const [categoryList, setCategoryList] = useState([]);
+  const [name, setname] = useState()
+  const [address, setAddress] = useState()
+  const [contact, setContact] = useState()
+  const [website, setWebsite] = useState()
+  const [about, setAbout] = useState()
+  const [category, setCategory] = useState()
+
+  const {user} = useUser()
 
   useEffect(() =>{
     navigation.setOptions({
       headerTitle: 'Add New Business'
     })
+    GetCategoryList()
   }, [])
 
   const onImagePick = async() => {
@@ -29,6 +42,53 @@ export default function AddBusiness() {
     console.log(result);
     
   }
+
+  const GetCategoryList = async() => {
+    setCategoryList([])
+    const q = query(collection(db,'colection'))
+    const snapShot = await getDocs(q)
+    snapShot.forEach((doc) => {
+      console.log(doc.data());
+      setCategoryList(prev=> [...prev, {
+        label: (doc.data()).name,
+        value: (doc.data()).value
+      }])
+    })
+  }
+
+  const onAddNewBus = async() => {
+    const fileName = Date.now().toString()+".jpg"
+    const res = await fetch(image)
+    const blob = await res.blob()
+
+    const imageRef = ref(storage, 'addBusiness/'+fileName)
+
+    uploadBytes(imageRef, blob).then((snapshot) => {
+      console.log("File uploaded");
+      
+    }).then(res=> {
+      getDownloadURL(imageRef).then(async(downloadUrl) => {
+        console.log(downloadUrl);
+        saveBusinessDetail(downloadUrl)
+      })
+    })
+  }
+
+  const saveBusinessDetail = async(imageUrl) => {
+    await setDoc(doc(db,'BusinessDetail',Date.now.toString()), {
+      name: name,
+      address: address,
+      contact: contact,
+      about: about,
+      website: website,
+      category: category,
+      username:user?.fullName,
+      userEmail: user?.primaryEmailAddress?.emailAddress,
+      userImage: user?.imageUrl,
+      imageUrl: imageUrl
+    })
+    ToastAndroid.show('New Business Added...',ToastAndroid.LONG)
+  }
   return (
     <View style={styles.container}>
       <ThemedText style={styles.title}>Add Business</ThemedText>
@@ -40,47 +100,51 @@ export default function AddBusiness() {
       </TouchableOpacity>
       <View>
         <TextInput
+        onChangeText={(v) => setname(v)}
         returnKeyType='next'
         inputMode='text'
         placeholder='Business Name'
         style={styles.input1}
          />
          <TextInput
+         onChangeText={(v) => setAddress(v)}
          returnKeyType='next'
         placeholder='Address'
         style={styles.input1}
          />
          <TextInput
+         onChangeText={(v) => setContact(v)}
          returnKeyType='next'
          inputMode='numeric'
         placeholder='Contact'
         style={styles.input1}
          />
          <TextInput
-         inputMode='email'
+         onChangeText={(v) => setWebsite(v)}
+         inputMode='url'
          returnKeyType='next'
-        placeholder='Email'
+        placeholder='Website'
         style={styles.input1}
          />
          <TextInput
+         onChangeText={(v) => setAbout(v)}
          returnKeyType='done'
          numberOfLines={5}
          multiline
         placeholder='About'
         style={[styles.input1, {height: 100}]}
          />
-         <View>
+         <View style={styles.selector}>
           <RNPickerSelect
-          onValueChange ={(value:any) => console.log(value)
+          onValueChange ={(value) => setCategory(value)
           }
-          items ={[
-            {label: 'Football', value: 'football'},
-            {label: 'Football', value: 'football'},
-            {label: 'Football', value: 'football'}
-          ]}
+          items ={categoryList}
            />
          </View>
       </View>
+      <TouchableOpacity onPress={() =>onAddNewBus()} style={styles.btn}>
+        <ThemedText style={{textAlign: 'center', fontFamily: 'outfit-meduim'}}>Add New Business</ThemedText>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -105,7 +169,7 @@ const styles = StyleSheet.create({
   },
   camFeed: {
     width: 200,
-    height: 200,
+    height: 150,
   },
   input1: {
     width: 'auto',
@@ -117,5 +181,19 @@ const styles = StyleSheet.create({
     fontSize: 17,
     marginTop: 10,
     fontFamily: 'outfit'
+  },
+  selector: {
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: Colors.GRAY,
+    fontFamily: 'outfit',
+    marginTop: 10,
+  },
+  btn: {
+    padding: 15,
+    backgroundColor: Colors.PRIMARY,
+    borderRadius: 6,
+    marginTop: 30
   }
 })
