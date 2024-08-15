40db66954b5e2cab694739846fe67d8d03e8d0e2
Joseph Acheampong <acheampongjoseph470@gmail.com>
2024-08-15 10:22:23 +0000

    update

--- app/business/my-business.tsx
+++ app/business/my-business.tsx
@@ -1,15 +1,29 @@
 import { View, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ThemedText } from '@/components/ThemedText'
 import { useUser } from '@clerk/clerk-expo'
-import { collection, query, where } from 'firebase/firestore'
+import { collection, doc, getDocs, query, where } from 'firebase/firestore'
 import { db } from '@/configs/FirebaseConfig'
 
 export default function MyBusiness() {
     const {user} = useUser()
 
-    const GetUserBus = () => {
-        const q = query(collection(db, 'BusinessList'), where)
+    const [businessList, setBusinessList] = useState()
+
+
+    useEffect(() => {
+        user&&GetUserBus()
+    }, [user])
+
+    const GetUserBus = async() => {
+        const q = query(collection(db, 'BusinessList'),where('userEmail', '==', user?.primaryEmailAddress?.emailAddress))
+
+        const querySnapshot = await getDocs(q)
+
+        querySnapshot.forEach((doc) => {
+            console.log(doc.data());
+            
+        })
     }
   return (
     <View style={styles.container}>
