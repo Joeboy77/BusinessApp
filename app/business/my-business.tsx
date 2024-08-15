2a4546a10a71ca781a493c6215f24772e354596c
Joseph Acheampong <acheampongjoseph470@gmail.com>
2024-08-15 10:27:54 +0000

    update

--- app/business/my-business.tsx
+++ app/business/my-business.tsx
@@ -1,14 +1,15 @@
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, FlatList } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { ThemedText } from '@/components/ThemedText'
 import { useUser } from '@clerk/clerk-expo'
 import { collection, doc, getDocs, query, where } from 'firebase/firestore'
 import { db } from '@/configs/FirebaseConfig'
+import BusinessListCard from '@/components/Explore/BusinessListCard'
 
 export default function MyBusiness() {
     const {user} = useUser()
 
-    const [businessList, setBusinessList] = useState()
+    const [businessList, setBusinessList] = useState([])
 
 
     useEffect(() => {
@@ -16,18 +17,25 @@ export default function MyBusiness() {
     }, [user])
 
     const GetUserBus = async() => {
+        setBusinessList([])
         const q = query(collection(db, 'BusinessList'),where('userEmail', '==', user?.primaryEmailAddress?.emailAddress))
 
         const querySnapshot = await getDocs(q)
 
         querySnapshot.forEach((doc) => {
             console.log(doc.data());
-            
+            setBusinessList(prev =>[...prev, {id:doc.id,...doc.data()}])
         })
     }
   return (
     <View style={styles.container}>
       <ThemedText style={{fontFamily: 'outfit-bold', fontSize: 25}}> My Business</ThemedText>
+      <FlatList
+      data={businessList}
+      renderItem={({item, index}) =>(
+        <BusinessListCard business={item} key={index}/>
+      )}
+       />
     </View>
   )
 }
